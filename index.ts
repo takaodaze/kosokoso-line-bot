94f298ae5b58601175d2158895b639a5d0fbd46e
takaodaze <gin2798@gmail.com>
2021-09-11 21:32:55 +0900

    add await

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import { handleRequest } from "./handleRequest.ts";
 
 // deno-lint-ignore no-explicit-any
 addEventListener("fetch", async (event: any) => {
-  event.respondWith(await handleRequest(event.request));
+  await event.respondWith(await handleRequest(event.request));
 });
